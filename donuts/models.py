747f86913f9d5c94e1ebf2cd6100a5b1e1691678
ereduvyge <tjay.tar@gmail.com>
2018-11-15 13:19:45 +0300

    First comm

---
+++ donuts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here
+
+class Donut(models.Model):
+    price=models.FloatField()
+    name=models.CharField(max_length=30)
+    onSale=models.BooleanField()
+    isSpecial=models.BooleanField()
+    description=models.TextField()
+    image=models.ImageField(upload_to='theme', default='theme/homer.jpg')
+
+    def __unicode__(self):
+        return self.name
+        return self.description
